3bed642de437fb40ad33c05c71d8dd47f587c404
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-11 20:47:35 +0100

    Countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ function countdown() {
 
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
+    mainEl.textContent = timeLeft
+    timeLeft--
     //
     // YOUR CODE HERE
     //
-  });
+  }, 1000);
 }
 
 // Displays the message one word at a time
